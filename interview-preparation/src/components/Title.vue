ce42443f0b1a5bffeede11a6f03c2ae00ca03cf5
burak zafer ozcan <ozcanburakzafer@gmail.com>
2024-06-27 02:52:13 +0300

    slots & named slots

--- interview-preparation/src/components/Title.vue
+++ interview-preparation/src/components/Title.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>{{ title }}</h1>
+  <!--? gönderilen slot name'i belirtilmemişse bu şekilde gösterilir  -->
+  <slot></slot>
+  <!--? gönderilen slot named slot ise bu şekilde gösterilir  -->
+  <slot name="title"></slot>
 </template>
 
 <script setup>
--- interview-preparation/src/views/Home.vue
+++ interview-preparation/src/views/Home.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <!--? child componente prop göndermek -->
-    <Title title="İnteview Preparation" />
+    <Title title="İnteview Preparation prop 1" />
     <Title :title="title2" />
+    <!--? child componente prop göndermek -->
+
+    <!--? slot mantığı -->
+    <Title>
+      <h1>İnteview Preparation slot mantığı</h1>
+    </Title>
+    <Title>
+      <h1 slot="title">İnteview Preparation named slot mantığı</h1>
+    </Title>
+    <!--? slot mantığı -->
     <Counter />
     <Product />
     <User />
@@ -17,5 +27,5 @@ import Counter from "../components/Counter.vue";
 import User from "../components/User.vue";
 import Product from "../components/Product.vue";
 import Test from "../components/Test.vue";
-const title2 = ref("İnteview Preparation2");
+const title2 = ref("İnteview Preparation prop 2");
 </script>
